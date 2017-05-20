84686ccb04b64895a35d6c014077f28a8b7c813d
Laura <l.castro12@uniandes.edu.co>
2017-05-19 21:55:54 -0500

    Fix

--- imports/components/ElGol.js
+++ imports/components/ElGol.js
@@ -12,24 +12,24 @@ export default class UserForm extends Component{
 				{this.props.estado}:
 				</h5>
 				<div className="form-group">
-				<label className="control-label col-sm-2" htmlFor="userR">Usuario:</label>
-				<div class="col-sm-8">
+				<label className="control-label col-sm-5" htmlFor="userR">Usuario:</label>
+				<div className="col-sm-11">
 				<input className="inputText form-control" id="userR" placeholder="Ingresa nombre de usuario"/>
 				</div>
 				</div>
 				<div className="form-group">
-				<label className="control-label col-sm-2" htmlFor="nombreR">Nombre:</label>
-				<div class="col-sm-8">
+				<label className="control-label col-sm-5" htmlFor="nombreR">Nombre:</label>
+				<div className="col-sm-11">
 				<input className="inputText form-control" id="nombreR" placeholder="Ingresa tu nombre"/>
 				</div>
 				</div>
 				<div className="form-group">
-				<label className="control-label col-sm-2" htmlFor="passR">Contraseña:</label>
-				<div class="col-sm-8">
+				<label className="control-label col-sm-5" htmlFor="passR">Contraseña:</label>
+				<div className="col-sm-11">
 				<input className="inputText form-control" id="passR" type="password" placeholder="Ingresa tu constraseña"/>
 				</div>
 				</div>
-				<div className="buttonsL" id="sig">
+				<div className="buttonsL col-sm-11" id="sig">
 				<button className="btn btn-primary"> Registrarse </button>
 				</div>
 				</div>
@@ -44,17 +44,17 @@ export default class UserForm extends Component{
 				</h5>
 				<div className="form-group">
 				<label className="control-label col-sm-2" htmlFor="userL">Usuario:</label>
-				<div class="col-sm-8">
+				<div className="col-sm-11">
 				<input className="inputText form-control" id="userL" placeholder="Ingresa nombre de usuario"/>
 				</div>
 				</div>
 				<div className="form-group">
 				<label className="control-label col-sm-2" htmlFor="passL">Contraseña:</label>
-				<div class="col-sm-8">
+				<div className="col-sm-11">
 				<input className="inputText form-control" id="passL" type="password" placeholder="Ingresa tu constraseña"/>
 				</div>
 				</div>
-				<div className="buttonsL" >
+				<div className="buttonsL col-sm-11" >
 				<button className="btn btn-primary"> Inicia Sesión </button>
 				</div>
 				</div>
