093824ed71a39240127b73acd4ecf56d57e7f234
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2017-05-21 16:39:05 -0500

    Cambios Heroku

--- imports/components/tusEquipos.js
+++ imports/components/tusEquipos.js
@@ -3,20 +3,43 @@ import React, { Component } from 'react';
 
 export default class TusEquipos extends Component {
 
+	logOut()
+	{
+		Meteor.logout((error) =>
+			{
+				if(error)
+				{
+
+				}
+				else
+				{
+					
+				}
+			});
+	}
+
 	render() {
 		return(
-			<div className="selectTeam">
+			<div className="selectTeam container">
+			<div className="tusEquipos">
+			<div className="tusEIzquierda">
 			<h1>Tus Equipos</h1>
+			</div>
+			<div className="tusEDerecha">
+				<button className="btn btn-danger" onClick={this.logOut.bind(this)}> Cerrar sesión</button>
+			</div>
+			</div>
+			<hr></hr>
 			<div className="dropdown">
 			<button className="dropbtn">Dropdown</button>
 			<div className="dropdown-content">
 			<a href="#">Link 1</a>
 			<a href="#">Link 2</a>
 			<a href="#">Link 3</a>
+			<button className="btn btn-success"> Añadir equipos </button>
 			</div>
 			</div>
-
 			</div>
-			)
+			);
 	}
 }
