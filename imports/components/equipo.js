ae32699d8cb6b85a9f363f4eeb07853265d3d9e8
Nicolas Vasquez <n.vasquez10@uniandes.edu.co>
2017-05-23 22:33:06 -0500

    Cambios

--- imports/components/equipo.js
+++ imports/components/equipo.js
@@ -9,7 +9,7 @@ export default class Equipos extends Component{
 	render()
 	{
 		return(
-			<div className="col-md-3 container equipo">
+			<div className="col-md-3 container gris">
 				<h5 className="tEquipo"> {this.props.equipo.Nombre}</h5>
 				<div className="centerImg">
 					<img className="img-responsive imgEquipo" src={this.props.equipo.url_escudo} title={this.props.equipo.Nombre} alt="foto equipo"/>
